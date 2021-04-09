18dd27a1f012b278e489037b1014c525a37be683
Carlos <moxiiru@gmail.com>
2021-04-09 18:33:22 -0300

    [ Finalizado ] Finalizado layout da janela.

--- web-application/src/pages/login/index.js
+++ web-application/src/pages/login/index.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import logomarca from '../../assets/images/logo-marca.png';
+
 import './styles.css';
 
 export default function Login () {
+    const [ user, setUser ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+    function handleLogin() {
+        alert("Seu email: " + user + "\nSua senha: " + password);
+    }
+
     return (
         <div className="container-body">
             <div className="container-login">
-                <form className="form-login">
+                <form className="form-login" onSubmit={ handleLogin } >
                     <img src={logomarca} alt=""/>
-                    <label>Endereço de E-mail</label>
-                    <input id="email" type="email" placeholder="ex: email@exemplo.com.br" />
-                    <label>senha</label>
-                    <input id="password" type="password" placeholder="Insira sua senha." />
-                    <button>Entrar</button>
+                    
+                    <label>Endereço de e-mail</label>
+                    <input 
+                        type="email" 
+                        value={ user }
+                        placeholder="ex: email@exemplo.com.br" 
+                        onChange={ e => setUser(e.target.value) }
+                    />
+
+                    <label>Senha</label>
+                    <input 
+                        type="password"
+                        value={ password }
+                        placeholder="Insira sua senha."
+                        onChange={ e => setPassword(e.target.value) }
+                    />
+                    <Link to="/register" >Esqueu sua senha?</Link>
+
+                    <button type="submit" >Entrar</button>
                 </form>
             </div>
-            <div className="container-image" >
-                Imagem de login
-            </div>
+            <div/>
         </div>
     )
 }
