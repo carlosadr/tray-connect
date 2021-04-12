a9d7b411990f211f8a45eed4bff17a5c3ffe5f5a
carlosa <carlosa.reboucas@gmail.com>
2021-04-12 07:26:18 -0300

    [ #5 ] Component Menu Global adicionado;

---
+++ web-application/src/components/MenuGlobal/index.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { FiDollarSign, FiHome, FiLayers, FiLayout, FiLogOut, FiPackage, FiTrello } from 'react-icons/fi'
+
+import logo from '../../assets/images/logo.png'
+
+import './styles.css';
+
+function MenuGlobal () {
+    return (
+        <nav className="container-nav">
+            <div className="container-buttons">
+                <img src={logo} alt="Tray Connect" className="container-img"/>
+                <NavLink 
+                    to="/#home"
+                    className="button"
+                    activeClassName="button-active"
+                >
+                    <FiHome size={18} />
+                    <div>
+                        Pagina Inicial
+                    </div>
+                </NavLink>
+
+                <NavLink 
+                    to="/storage"
+                    className="button"
+                    activeClassName="button-active"
+                >
+                    <FiPackage size={18} />
+                    <div>
+                        Estoque
+                    </div>
+                </NavLink>
+                
+                <NavLink 
+                    to="/development"
+                    className="button"
+                    activeClassName="button-active"
+                >
+                    <FiLayers size={18} />
+                    <div>
+                        Desenvolvimento
+                    </div>
+                </NavLink>
+                
+                <NavLink 
+                    to="/commercial"
+                    className="button"
+                    activeClassName="button-active"
+                >
+                    <FiLayout size={18} />
+                    <div>
+                        Comercial
+                    </div>
+                </NavLink>
+                
+                <NavLink 
+                    to="/production"
+                    className="button"
+                    activeClassName="button-active"
+                >
+                    <FiTrello size={18} />
+                    <div>
+                        Produção
+                    </div>
+                </NavLink>
+                
+                <NavLink 
+                    to="/financial"
+                    className="button"
+                    activeClassName="button-active"
+                >
+                    <FiDollarSign size={18} />
+                    <div>
+                        Financeiro
+                    </div>
+                </NavLink>
+            </div>
+            <div className="container-singout" >
+                <Link className="button">
+                    <FiLogOut size={18} color={"#44475A"} />
+                    <div>
+                        Sair
+                    </div>
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default MenuGlobal;
