e7b95d5a8656d58d90b690adc8866e40771f2981
Carlos <moxiiru@gmail.com>
2021-04-13 17:15:17 -0300

    [ Finalizado ] - Tela de Estoque Completa

--- web-application/src/pages/Application/Storage/index.js
+++ web-application/src/pages/Application/Storage/index.js
@@ -25,18 +25,107 @@ export default function Storage () {
                     </>
                 }>
                     <TabContant id="Todos">
-                        <h1>
-                            TODOS ELEMENTOS
-                        </h1>
+                        <table id="table-data" className="container-table">
+                            <tbody className="container-table-body">
+                                <tr className="header-table">
+                                    <td style={{ display : 'none' }} className="col-id">
+                                        id
+                                    </td>
+                                    <td className="col started">
+                                        Entrada
+                                    </td>
+                                    <td className="col roll">
+                                        Rolo
+                                    </td>
+                                    <td className="col date-started">
+                                        Data de entrada
+                                    </td>
+                                    <td className="col client">
+                                        Cliente
+                                    </td>
+                                    <td className="col reference">
+                                        Referencia
+                                    </td>
+                                    <td className="col description">
+                                        Descrição
+                                    </td>
+                                    <td className="col type">
+                                        Tipo
+                                    </td>
+                                    <td className="col type-fabtic">
+                                        Tipo do Tecido
+                                    </td>
+                                    <td className="col color-fabric">
+                                        Cor do Tecido
+                                    </td>
+                                    <td className="col width-grid">
+                                        Largura / Grade
+                                    </td>
+                                    <td className="col metric-unid">
+                                        Quantidade
+                                    </td>
+                                    <td className="col review">
+                                        Revisão
+                                    </td>
+                                    <td className="col actions">
+                                        Ações
+                                    </td>
+                                </tr>
+                                <tr className="table-rows">
+                                    <td style={{ display : 'none' }} className="col-id">
+                                        id
+                                    </td>
+                                    <td className="col started">
+                                        5542
+                                    </td>
+                                    <td className="col roll">
+                                        1
+                                    </td>
+                                    <td className="col date-started">
+                                        22/04/2020
+                                    </td>
+                                    <td className="col client">
+                                        Claudinho e Bochecha
+                                    </td>
+                                    <td className="col reference">
+                                        SGS220
+                                    </td>
+                                    <td className="col description">
+                                        -
+                                    </td>
+                                    <td className="col type">
+                                        MT
+                                    </td>
+                                    <td className="col type-fabtic">
+                                        Tactel
+                                    </td>
+                                    <td className="col color-fabric">
+                                        Branco
+                                    </td>
+                                    <td className="col width-grid">
+                                        1.60
+                                    </td>
+                                    <td className="col metric-unid">
+                                        240
+                                    </td>
+                                    <td className="col review">
+                                        Não
+                                    </td>
+                                    <td className="col actions">
+                                        Editar  |   Excluir
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </TabContant>
                     <TabContant id="Clientes">
                         <h1>
-                        CLIENTES
+                            Estoque de Clientes
                         </h1>
                     </TabContant>
                     <TabContant id="Fabrica">
                         <h1>
-                        ESTOQUE de Fabrica
+                            Estoque da fábrica
                         </h1>
                     </TabContant>
                 </TabsContainer>
