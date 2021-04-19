331ec04edfd4139c0d5dbdf51305fedfad83681a
Carlos <moxiiru@gmail.com>
2021-04-19 16:57:29 -0300

    ADD botao de expançao do Menu Global;

--- web-application/src/components/MenuGlobal/index.js
+++ web-application/src/components/MenuGlobal/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import { FiDollarSign, FiHome, FiLayers, FiLayout, FiLogOut, FiPackage, FiTrello } from 'react-icons/fi';
+import { FiChevronRight, FiDollarSign, FiHome, FiLayers, FiLayout, FiLogOut, FiPackage, FiTrello } from 'react-icons/fi';
 
 import firebase from 'firebase';
 import 'firebase/auth';
@@ -10,14 +10,27 @@ import logo from '../../assets/images/logo.png'
 import './styles.css';
 
 function MenuGlobal () {
+    const [visible, setVisible] = useState('none');
+    const [maxWidth, setMaxWidth] = useState('82px');
+    const [widthImag, setWidthImag] = useState('100%');
+    const [rotate, setRotate] = useState('rotate(0deg)');
+
     function handleSingout() {
+        localStorage.clear()
         firebase.auth().signOut()
+    }
+    
+    function handleExpander () {
+        visible === 'none' ? setVisible('flex') : setVisible('none');
+        maxWidth === '82px' ? setMaxWidth('240px') : setMaxWidth('82px');
+        widthImag === '100%' ? setWidthImag('40%') : setWidthImag('100%');
+        rotate === 'rotate(0deg)' ? setRotate('rotate(180deg)') : setRotate('rotate(0deg)');
     }
 
     return (
-        <nav className="container-nav">
+        <nav style={{ maxWidth : maxWidth }} className="container-nav">
             <div className="container-buttons">
-                <img src={logo} alt="Tray Connect" className="container-img"/>
+                <img style={{ width : widthImag }} src={ logo } alt="Tray Connect" className="container-img"/>
                 <NavLink 
                     to="/"
                     exact
@@ -25,7 +38,7 @@ function MenuGlobal () {
                     activeClassName="button-active"
                 >
                     <FiHome size={18} />
-                    <div>
+                    <div style={{ display : visible }} >
                         Pagina Inicial
                     </div>
                 </NavLink>
@@ -36,7 +49,7 @@ function MenuGlobal () {
                     activeClassName="button-active"
                 >
                     <FiPackage size={18} />
-                    <div>
+                    <div style={{ display : visible }} >
                         Estoque
                     </div>
                 </NavLink>
@@ -47,7 +60,7 @@ function MenuGlobal () {
                     activeClassName="button-active"
                 >
                     <FiLayers size={18} />
-                    <div>
+                    <div style={{ display : visible }} >
                         Desenvolvimento
                     </div>
                 </NavLink>
@@ -58,7 +71,7 @@ function MenuGlobal () {
                     activeClassName="button-active"
                 >
                     <FiLayout size={18} />
-                    <div>
+                    <div style={{ display : visible }} >
                         Comercial
                     </div>
                 </NavLink>
@@ -69,7 +82,7 @@ function MenuGlobal () {
                     activeClassName="button-active"
                 >
                     <FiTrello size={18} />
-                    <div>
+                    <div style={{ display : visible }} >
                         Produção
                     </div>
                 </NavLink>
@@ -80,7 +93,7 @@ function MenuGlobal () {
                     activeClassName="button-active"
                 >
                     <FiDollarSign size={18} />
-                    <div>
+                    <div style={{ display : visible }} >
                         Financeiro
                     </div>
                 </NavLink>
@@ -92,11 +105,14 @@ function MenuGlobal () {
                     onClick={ () => handleSingout() }
                 >
                     <FiLogOut size={18} color={"#44475A"} />
-                    <div>
+                    <div style={{ display : visible }} >
                         Sair
                     </div>
                 </Link>
             </div>
+            <button style={{ transform : rotate }} className="button-expander" onClick={ () => handleExpander() }>
+                <FiChevronRight size={16} color={'#000'} />
+            </button>
         </nav>
     )
 }
