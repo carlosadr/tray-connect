ff59139f61c314adcda26391f270a589aee8bf13
Carlos <moxiiru@gmail.com>
2021-04-20 11:36:32 -0300

    Ajustes na pasta "Public"

--- web-application/src/index.js
+++ web-application/src/index.js
@@ -2,21 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import './services/api'
+
 import './global.css';
-
-import firebase from "firebase/app";
-
-// Use your config values here.
-firebase.initializeApp({
-  apiKey: "AIzaSyAYgDIgDlUF2GoleDUnmNX4Vt7_gOCKXyk",
-  authDomain: "tray-connect-systems.firebaseapp.com",
-  databaseURL: "https://tray-connect-systems-default-rtdb.firebaseio.com",
-  projectId: "tray-connect-systems",
-  storageBucket: "tray-connect-systems.appspot.com",
-  messagingSenderId: "3344673671",
-  appId: "1:3344673671:web:5f28412bdbc292dbb78122",
-  measurementId: "G-1V8K3SHQ8S"
-});
 
 ReactDOM.render(
   <React.StrictMode>
